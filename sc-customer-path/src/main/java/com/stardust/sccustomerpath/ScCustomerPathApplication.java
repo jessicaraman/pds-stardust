34466bc758d2acd0b2c4158b8c2297364f5a755b
IliesFaddaoui <ilies.faddaoui@gmail.com>
2020-01-24 10:06:42 +0100

    #TECH: move customer path ms to repo + diagrams

---
+++ sc-customer-path/src/main/java/com/stardust/sccustomerpath/ScCustomerPathApplication.java
@@ -0,0 +1,13 @@
+package com.stardust.sccustomerpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScCustomerPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScCustomerPathApplication.class, args);
+	}
+
+}
---
+++ sc-customer-path/src/test/java/com/stardust/sccustomerpath/ScCustomerPathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stardust.sccustomerpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScCustomerPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
