befb4fc1aee27c36bdbba30fd54b4ab61668c835
IliesFaddaoui <ilies.faddaoui@gmail.com>
2020-01-24 23:18:31 +0100

    US customerPath: udpate swagger + create controller + first connection with index.js + db connection with mongo okay

--- sc-customer-path/index.js
+++ sc-customer-path/index.js
@@ -4,26 +4,25 @@ var express = require("express"),
 var fs = require("fs");
 var mongoose = require('mongoose');
 var bodyParser = require("body-parser");
+var customerPathController = require('./controllers/customerPathController.js')
+var mapController = require('./controllers/mapController.js')
+
 app.listen(port, function(){
     console.log('App run on port '+ port);
 });
 
 app.get('/', (req, res) => res.send('Hello World with Express'));
-/*app.get('/getProductById', (req, res, next) => {
-    cproduct.findProductById;
+app.get('/map', (req, res, next) => {
+    mapController.getMallMap;
 });
 
-app.post('/postProduct', (req, res, next) => {
-    cproduct.createProduct;
-})
-app.delete('/deleteProduct', (req, res, next) => {
-    cproduct.deleteProductById;
-})
-
+app.get('/customerPath', (req, res, next) => {
+    customerPathController.getCustomerPath;
+});
 
 mongoose.connect('mongodb://localhost/db').then(() => {
     console.log('Connected to mongoDB')
 }).catch(e => {
     console.log('Error while DB connecting');
     console.log(e);
-});*/
+});
