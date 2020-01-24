5512b007a4ef2bb971bc117bb9da55c740f7423f
samuel.lebon <samuel.lebon@etu.u-pec.fr>
2020-01-24 13:14:56 +0100

    Add geolocalisation activity

---
+++ pgmapp/app/src/main/java/com/pds/pgmapp/activity/GeolocalisationActivity.java
@@ -0,0 +1,16 @@
+package com.pds.pgmapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pds.pgmapp.R;
+
+public class GeolocalisationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geolocalisation);
+    }
+}
