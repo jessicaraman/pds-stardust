fbf1d216d546d814b04f03abbe92960065d29856
Damien <damien.baraton@hotmail.fr>
2020-01-24 12:04:49 +0100

    [Java class] Added Node.java

--- pgmapp/app/src/main/java/com/pds/pgmapp/model/Node.java
+++ pgmapp/app/src/main/java/com/pds/pgmapp/model/Node.java
@@ -1,10 +1,24 @@
-package com.pds.pgmapp;
+package com.pds.pgmapp.model;
 
+/**
+ * Node entity
+ */
 public class Node {
+
+    /**
+     * Properties
+     */
     int x, y;
     String direction;
 
-    public Node (int x, int y, String direction) {
+    /**
+     * Constructor
+     *
+     * @param x
+     * @param y
+     * @param direction
+     */
+    public Node(int x, int y, String direction) {
         this.x = x;
         this.y = y;
         this.direction = direction;
