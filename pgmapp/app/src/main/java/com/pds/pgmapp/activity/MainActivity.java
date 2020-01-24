c49f63d9c83b2ce0b6ec218209101d351ff161e7
Damien <damien.baraton@hotmail.fr>
2020-01-24 12:04:16 +0100

    [Activity] added MainActivty

--- pgmapp/app/src/main/java/com/pds/pgmapp/activity/MainActivity.java
+++ pgmapp/app/src/main/java/com/pds/pgmapp/activity/MainActivity.java
@@ -1,14 +1,31 @@
-package com.pds.pgmapp;
+package com.pds.pgmapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
+import com.pds.pgmapp.R;
+
+/**
+ * MainActivity : HomePage
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonUserGuideStoreAction();
+    }
+
+    /**
+     * Access to countries list
+     */
+    protected void buttonUserGuideStoreAction() {
+        Button buttonCountriesList;
+        buttonCountriesList = findViewById(R.id.buttonGuideUserStore);
+        buttonCountriesList.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),GuideUserStoreActivity.class)));
     }
 }
