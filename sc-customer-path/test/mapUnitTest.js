d062c99d9a4a8922c2d2dd009f4dcc512406c328
IliesFaddaoui <ilies.faddaoui@gmail.com>
2020-01-25 23:36:35 +0100

    US customerpath: add empty unit tests + update controllers

---
+++ sc-customer-path/test/mapUnitTest.js
@@ -0,0 +1,8 @@
+/**
+ * Empty unit tests on customer path to be completed during the dev of the features:
+ */
+var assert = require('assert');
+describe('map', function() {
+    describe('#getCustomerPath()', function(){
+    });
+});
