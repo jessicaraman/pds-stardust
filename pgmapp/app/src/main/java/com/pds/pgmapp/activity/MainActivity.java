9af0d502062e4be080405a95c0ea35c966eed37f
samuel.lebon <samuel.lebon@etu.u-pec.fr>
2020-02-04 16:55:31 +0100

    [geolocation] Update main activity

--- pgmapp/app/src/main/java/com/pds/pgmapp/activity/MainActivity.java
+++ pgmapp/app/src/main/java/com/pds/pgmapp/activity/MainActivity.java
@@ -1,8 +1,11 @@
 package com.pds.pgmapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -17,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            int MY_PERMISSIONS_ACCESS_COARSE_LOCATION = 1;
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_ACCESS_COARSE_LOCATION
+            );
+        }
+
         buttonUserGuideStoreAction();
         startGeolocatisation();
     }
@@ -33,6 +45,6 @@ public class MainActivity extends AppCompatActivity {
     protected void startGeolocatisation() {
         Button startGeolocalisationBtn;
         startGeolocalisationBtn = findViewById(R.id.buttonGeolocalisation);
-        startGeolocalisationBtn.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), GeolocalisationActivity.class)));
+        startGeolocalisationBtn.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), LocationActivity.class)));
     }
 }
