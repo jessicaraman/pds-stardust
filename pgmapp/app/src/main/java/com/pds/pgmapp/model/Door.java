96f99cad4860ec7c834f483d8003cda55ec01396
Suriya Sammandamourthy <s.suriya@outlook.fr>
2020-02-05 21:50:41 +0100

    Add Door entity class

---
+++ pgmapp/app/src/main/java/com/pds/pgmapp/model/Door.java
@@ -0,0 +1,37 @@
+package com.pds.pgmapp.model;
+
+public class Door {
+    private int id;
+    private double latitude;
+    private double longitude;
+
+    public Door(int id, double latitude, double longitude) {
+        this.id = id;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
