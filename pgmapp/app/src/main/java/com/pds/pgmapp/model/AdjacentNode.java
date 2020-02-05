b2c0b6e5311f52aad15e5b7517b94629380afa7b
Suriya Sammandamourthy <s.suriya@outlook.fr>
2020-02-05 21:50:16 +0100

    Add AdjacentNode entity class

---
+++ pgmapp/app/src/main/java/com/pds/pgmapp/model/AdjacentNode.java
@@ -0,0 +1,41 @@
+package com.pds.pgmapp.model;
+
+public class AdjacentNode {
+
+    private int mainNodeId;
+    private int adjacentNodeId;
+    private double distance;
+
+    public AdjacentNode(int mainNodeId, int adjacentNodeId, double distance) {
+        this.mainNodeId = mainNodeId;
+        this.adjacentNodeId = adjacentNodeId;
+        this.distance = distance;
+    }
+
+    public AdjacentNode() {
+    }
+
+    public int getMainNodeId() {
+        return mainNodeId;
+    }
+
+    public void setMainNodeId(int mainNodeId) {
+        this.mainNodeId = mainNodeId;
+    }
+
+    public int getAdjacentNodeId() {
+        return adjacentNodeId;
+    }
+
+    public void setAdjacentNodeId(int adjacentNodeId) {
+        this.adjacentNodeId = adjacentNodeId;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+}
