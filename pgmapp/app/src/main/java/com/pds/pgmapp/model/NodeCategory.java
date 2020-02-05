d444b9cb6a032d2378fb5a3a5a1cb1b55a347503
Suriya Sammandamourthy <s.suriya@outlook.fr>
2020-02-05 21:51:09 +0100

    Add NodeCategory entity class

---
+++ pgmapp/app/src/main/java/com/pds/pgmapp/model/NodeCategory.java
@@ -0,0 +1,29 @@
+package com.pds.pgmapp.model;
+
+public class NodeCategory {
+    private int id;
+    private String label;
+
+    public NodeCategory(int id, String label) {
+        this.id = id;
+        this.label = label;
+    }
+
+    public NodeCategory () {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
