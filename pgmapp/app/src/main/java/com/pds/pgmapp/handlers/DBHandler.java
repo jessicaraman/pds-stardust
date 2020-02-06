39ff45f8f07a8296946d2cc737b7ccf2bac840a8
Suriya Sammandamourthy <s.suriya@outlook.fr>
2020-02-06 19:07:34 +0100

    Adding logs

--- pgmapp/app/src/main/java/com/pds/pgmapp/handlers/DBHandler.java
+++ pgmapp/app/src/main/java/com/pds/pgmapp/handlers/DBHandler.java
@@ -15,7 +15,6 @@ import com.pds.pgmapp.model.Door;
 import com.pds.pgmapp.model.Node;
 import com.pds.pgmapp.model.NodeCategory;
 
-import java.lang.reflect.Array;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,74 +24,41 @@ import java.util.List;
  */
 public class DBHandler extends SQLiteOpenHelper {
 
-    private static DBHandler DBInstance;
-
-    /**
-     * Singleton
-     * @param context
-     * @return
-     */
-    public static synchronized DBHandler getInstance(Context context) {
-        System.out.println("Getting INSTANCE");
-        if (DBInstance == null) {
-            System.out.println("WWWWWWWWWWWWWWWWWWWWWWWWWWww");
-            DBInstance = new DBHandler(context.getApplicationContext());
-        }
-        return DBInstance;
-    }
-
-    /**
-     * Constructor should be private to prevent direct instantiation.
-     * make call to static method "getInstance()" instead.
-     */
-    private DBHandler(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        this.db = getWritableDatabase();
-        System.out.println("Getting HANDLER");
-    }
-
-    SQLiteDatabase db ;
-
     // database version
     private static final int DATABASE_VERSION = 1;
-
     // database name
     private static final String DATABASE_NAME = "pds";
-
     // tables
     private static final String TABLE_LOCATION = "locations";
     private static final String TABLE_NODE = "node";
     private static final String TABLE_NODE_CATEGORY = "nodecategory";
     private static final String TABLE_ADJACENT_NODE = "adjacentnode";
     private static final String TABLE_DOOR = "door";
-
     // location properties
     private static final String KEY_LOCATION_X = "location_x";
     private static final String KEY_LOCATION_Y = "location_y";
     private static final String KEY_LOCATION_DATE = "location_date";
-
     //  node properties
     private static final String KEY_NODE_ID = "id";
     private static final String KEY_NODE_LABEL = "label";
     private static final String KEY_NODE_CATEGORY = "node_category";
-
     // node category properties
     private static final String KEY_NODE_CATEGORY_ID = "id";
     private static final String KEY_NODE_CATEGORY_LABEL = "label";
-
     // node adjacent properties
     private static final String KEY_NODE_ADJACENT_MAIN_NODE_ID = "main_node_id";
     private static final String KEY_NODE_ADJACENT_ADJACENT_NODE_ID = "adjacent_node_id";
     private static final String KEY_NODE_ADJACENT_DISTANCE = "main_adjacent_node_distance";
-
     // door properties
     private static final String KEY_DOOR_ID = "id";
     private static final String KEY_DOOR_X = "x";
     private static final String KEY_DOOR_Y = "y";
     private static final String KEY_DOOR_ID_NODE = "id_node";
+    private static DBHandler DBInstance;
+    SQLiteDatabase db;
 
     // create table locations
-   private String CREATE_LOCATIONS_TABLE = "CREATE TABLE IF NOT EXISTS "
+    private String CREATE_LOCATIONS_TABLE = "CREATE TABLE IF NOT EXISTS "
             + TABLE_LOCATION + "("
             + KEY_LOCATION_X + " TEXT, "
             + KEY_LOCATION_Y + " TEXT, "
@@ -126,32 +92,64 @@ public class DBHandler extends SQLiteOpenHelper {
             + KEY_DOOR_Y + " TEXT, "
             + KEY_DOOR_ID_NODE + " INTEGER" + ")";
 
+    /**
+     * Constructor should be private to prevent direct instantiation.
+     * make call to static method "getInstance()" instead.
+     */
+    private DBHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.db = getWritableDatabase();
+        Log.e("log", "Getting HANDLER");
+    }
+
+    /**
+     * Singleton
+     *
+     * @param context
+     * @return
+     */
+    public static synchronized DBHandler getInstance(Context context) {
+        Log.e("log", "Getting INSTANCE");
+        if (DBInstance == null) {
+            DBInstance = new DBHandler(context.getApplicationContext());
+        }
+        return DBInstance;
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        Log.e("", "DB onCreate beginning");
-        //Query to create table
-        System.out.println("CREATING TABLES");
+        Log.e("log", "DB onCreate beginning");
+
+        // Queries to create table
+        Log.e("log", "CREATION TABLES");
         db.execSQL(CREATE_LOCATIONS_TABLE);
         db.execSQL(CREATE_NODE_CATEGORY_TABLE);
         db.execSQL(CREATE_NODE_TABLE);
         db.execSQL(CREATE_ADJACENT_NODE_TABLE);
         db.execSQL(CREATE_DOOR_TABLE);
+
+        // Queries mocks
+        Log.e("log", "INSERT DATAS");
         insertNodeCategory(db);
         insertNode(db);
         insertDoor(db);
         insertAdjacentNode(db);
-        Log.e("", "DB onCreate end") ;
+
+        Log.e("log", "DB onCreate end");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_LOCATION);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ADJACENT_NODE);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_DOOR);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NODE);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NODE_CATEGORY);
         onCreate(db);
     }
 
     public void truncateLocation() {
         SQLiteDatabase db = this.getWritableDatabase();
-
         db.execSQL("DELETE FROM " + TABLE_LOCATION);
     }
 
@@ -216,39 +214,22 @@ public class DBHandler extends SQLiteOpenHelper {
         return locations;
     }
 
-    /*public Node getNodeById (int id) {
-        SQLiteDatabase db = this.getReadableDatabase();
-
-        String selectQuery = "SELECT  * FROM " + TABLE_NODE + " WHERE "
-                + KEY_NODE_ID + " = " + id;
-
-        Log.e("log", selectQuery);
-
-        Cursor c = db.rawQuery(selectQuery, null);
-
-        if (c != null)
-            c.moveToFirst();
-
-        Node node = new Node();
-        node.setId(c.getInt(c.getColumnIndex(KEY_NODE_ID)));
-        node.setLabel((c.getString(c.getColumnIndex(KEY_NODE_LABEL))));
-        node.setNodeCategory(new NodeCategory(c.getString(c.getColumnIndex(KEY_NODE_CATEGORY))));
-
-        return node;
-    }*/
-
-    public Node getNodeById (int id) {
+    /**
+     * Get Node informations by id
+     *
+     * @param id
+     * @return
+     */
+    public Node getNodeById(int id) {
         SQLiteDatabase db = this.getReadableDatabase();
         String selectNodesQuery = "SELECT * FROM " + TABLE_NODE + " WHERE " + KEY_NODE_ID + " = " + id + ";";
-        String selectDoorsQuery = "SELECT * FROM " + TABLE_DOOR + " WHERE " + KEY_DOOR_ID_NODE + " = " + id ;
-        Log.e("","get Writable Database");
-        //SQLiteDatabase db = this.getWritableDatabase();
+        String selectDoorsQuery = "SELECT * FROM " + TABLE_DOOR + " WHERE " + KEY_DOOR_ID_NODE + " = " + id;
         Cursor cursorNodes = db.rawQuery(selectNodesQuery, null);
         Cursor cursorDoors = db.rawQuery(selectDoorsQuery, null);
 
-        Node node = null ;
-        NodeCategory nodeCategory = null ;
-        ArrayList<Door> doors = new ArrayList<Door> () ;
+        Node node = null;
+        NodeCategory nodeCategory = null;
+        ArrayList<Door> doors = new ArrayList<Door>();
 
         if (cursorDoors.moveToFirst()) {
             do {
@@ -263,15 +244,15 @@ public class DBHandler extends SQLiteOpenHelper {
         }
 
         if (cursorNodes.moveToFirst()) {
-            String selectNodeCategoryQuery = " SELECT * FROM " + TABLE_NODE_CATEGORY + " WHERE id = " + cursorNodes.getInt(2) ;
+            String selectNodeCategoryQuery = " SELECT * FROM " + TABLE_NODE_CATEGORY + " WHERE id = " + cursorNodes.getInt(2);
             Cursor cursorNodeCategory = db.rawQuery(selectNodeCategoryQuery, null);
             //nodeCategory = new NodeCategory(cursorNodeCategory.getInt(0), cursorNodeCategory.getString(1));
-            nodeCategory = new NodeCategory(1,"test");
+            nodeCategory = new NodeCategory(1, "test");
             node = new Node(
-                cursorNodes.getInt(0),
-                cursorNodes.getString(1),
-                nodeCategory,
-                doors);
+                    cursorNodes.getInt(0),
+                    cursorNodes.getString(1),
+                    nodeCategory,
+                    doors);
         }
         return node;
     }
@@ -282,7 +263,7 @@ public class DBHandler extends SQLiteOpenHelper {
      * @param db
      */
     public void insertNode(SQLiteDatabase db) {
-        Log.e("","insertNode");
+        Log.e("log", "Insert Node");
         db.execSQL("INSERT INTO node (id,label,node_category) VALUES (1,'Nike',2);");
         db.execSQL("INSERT INTO node (id,label,node_category) VALUES (2,'Adidas',2);");
         db.execSQL("INSERT INTO node (id,label,node_category) VALUES (3,'JD Sports',2);");
@@ -297,7 +278,7 @@ public class DBHandler extends SQLiteOpenHelper {
      * @param db
      */
     public void insertNodeCategory(SQLiteDatabase db) {
-        Log.e("log", "insert Node Category");
+        Log.e("log", "Insert Node Category");
         db.execSQL("INSERT INTO nodecategory (id,label) VALUES (1, 'Corridor');");
         db.execSQL("INSERT INTO nodecategory (id,label) VALUES (2, 'Store');");
     }
@@ -308,7 +289,7 @@ public class DBHandler extends SQLiteOpenHelper {
      * @param db
      */
     public void insertAdjacentNode(SQLiteDatabase db) {
-        Log.e("log", "insertAdjacentNode");
+        Log.e("log", "Insert AdjacentNode");
         db.execSQL("INSERT INTO adjacentnode (main_node_id, adjacent_node_id, main_adjacent_node_distance) VALUES (1,2, 132);");
         db.execSQL("INSERT INTO adjacentnode (main_node_id, adjacent_node_id, main_adjacent_node_distance) VALUES (5,6, 242);");
         db.execSQL("INSERT INTO adjacentnode (main_node_id, adjacent_node_id, main_adjacent_node_distance) VALUES (3,2,1343);");
@@ -324,7 +305,7 @@ public class DBHandler extends SQLiteOpenHelper {
      * @param db
      */
     public void insertDoor(SQLiteDatabase db) {
-        Log.e("log", "insertDoor");
+        Log.e("log", "Insert Door");
         db.execSQL("INSERT INTO door (id,x,y,id_node) VALUES (1, 0.2921868823996539, 0.7736856476831694, 1)");
         db.execSQL("INSERT INTO door (id,x,y,id_node) VALUES (2, 0.5438542339380292, 0.1838274390274859, 2)");
         db.execSQL("INSERT INTO door (id,x,y,id_node) VALUES (3, 0.4748390248790389, 0.6430892890328030, 3)");
