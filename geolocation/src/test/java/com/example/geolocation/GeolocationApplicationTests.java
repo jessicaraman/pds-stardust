d7b61c4467e2f8649433b8a5ca8f37df846eb268
samuel.lebon <samuel.lebon@etu.u-pec.fr>
2020-02-23 12:09:40 +0100

    [geolocation] Add minimal geolocation project

---
+++ geolocation/src/test/java/com/example/geolocation/GeolocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geolocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeolocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
