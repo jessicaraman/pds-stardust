f74a1d33b5370e944e333a03fdc50259e1a2a569
samuel.lebon <samuel.lebon@etu.u-pec.fr>
2020-02-23 12:04:01 +0100

    [geolocation] Add location history service

---
+++ geolocation/src/main/java/com/example/geolocation/services/LocationHistoryService.java
@@ -0,0 +1,41 @@
+package com.example.geolocation.services;
+
+import com.example.geolocation.entities.LocationEntity;
+import com.example.geolocation.entities.LocationHistoryEntity;
+import com.example.geolocation.repositories.LocationHistoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LocationHistoryService {
+
+    private final LocationHistoryRepository locationHistoryRepository;
+
+    @Autowired
+    public LocationHistoryService(LocationHistoryRepository locationHistoryRepository) {
+        this.locationHistoryRepository = locationHistoryRepository;
+    }
+
+    public LocationHistoryEntity save(LocationHistoryEntity locationHistory) {
+
+        Optional<LocationHistoryEntity> optLocationHistoryInDb= findById(locationHistory.getId());
+
+        if (optLocationHistoryInDb.isPresent()) {
+            LocationHistoryEntity locationHistoryInDb = optLocationHistoryInDb.get();
+            List<LocationEntity> locations = locationHistoryInDb.getLocations();
+            locations.addAll(locationHistory.getLocations());
+            locationHistoryInDb.setLocations(locations);
+
+            return locationHistoryRepository.save(locationHistoryInDb);
+        }
+
+        return locationHistoryRepository.save(locationHistory);
+    }
+
+    public Optional<LocationHistoryEntity> findById(String id) {
+        return locationHistoryRepository.findById(id);
+    }
+}
