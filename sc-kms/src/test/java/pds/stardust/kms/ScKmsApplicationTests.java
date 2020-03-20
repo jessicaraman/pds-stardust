5048638a700b6e1c2f83a43bb3e743773c9e2e68
samuel.lebon <samuel.lebon@etu.u-pec.fr>
2020-03-20 20:02:05 +0100

    [SC-KMS] Add key management system project

---
+++ sc-kms/src/test/java/pds/stardust/kms/ScKmsApplicationTests.java
@@ -0,0 +1,13 @@
+package pds.stardust.kms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScKmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
