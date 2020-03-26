1bad0cb19796ac912ca8d83cd8c9629297ed3b86
samuel.lebon <samuel.lebon@etu.u-pec.fr>
2020-03-26 22:01:06 +0100

    [EM] SC-KMS : Update application config

--- sc-kms/src/main/java/pds/stardust/kms/ScKmsApplication.java
+++ sc-kms/src/main/java/pds/stardust/kms/ScKmsApplication.java
@@ -1,10 +1,17 @@
 package pds.stardust.kms;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.Security;
+
 @SpringBootApplication
 public class ScKmsApplication {
+
+	static {
+		Security.addProvider(new BouncyCastleProvider());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScKmsApplication.class, args);
