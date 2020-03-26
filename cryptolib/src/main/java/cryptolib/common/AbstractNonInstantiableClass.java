9aea7b6bc30ef7bb426d66b6e5df744f3fe7c018
samuel.lebon <samuel.lebon@etu.u-pec.fr>
2020-03-26 04:04:47 +0100

    [CRYPTOLIB] Add abstract non instantiable class

---
+++ cryptolib/src/main/java/cryptolib/common/AbstractNonInstantiableClass.java
@@ -0,0 +1,13 @@
+package cryptolib.common;
+
+/**
+ * This class defines the common variables and methods that will be shared by utility classes.
+ */
+public abstract class AbstractNonInstantiableClass {
+
+    /**
+     * Defines the exception message that will be shown in case of an utility class being instantiated.
+     */
+    public static final String NON_INSTANTIABLE_CLASS_INSTANTIATION_EXCEPTION = "Utility class should not be instantiated";
+
+}
