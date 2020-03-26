158289e134d20f1037ca4028b7164d0a65d9896e
samuel.lebon <samuel.lebon@etu.u-pec.fr>
2020-03-26 21:42:51 +0100

    [EM] Geolocation : Update application config

--- geolocation/src/main/java/com/example/geolocation/GeolocationApplication.java
+++ geolocation/src/main/java/com/example/geolocation/GeolocationApplication.java
@@ -1,10 +1,19 @@
 package com.example.geolocation;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.Security;
+
 @SpringBootApplication
+@EnableEncryptableProperties
 public class GeolocationApplication {
+
+	static {
+		Security.addProvider(new BouncyCastleProvider());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GeolocationApplication.class, args);
