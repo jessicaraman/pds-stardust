d34349f288c5a27a5e9000e08a15aad6c83c7e55
lobiyed.karim <lobiyed.karim@gmail.com>
2020-04-07 22:12:28 +0200

    [frequentation] init

---
+++ frequentation/src/test/java/pds/stardust/frequentation/FrequentationApplicationTests.java
@@ -0,0 +1,13 @@
+package pds.stardust.frequentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrequentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
