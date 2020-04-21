4d4d4fe3078015b8c4f8a35624909400515c3ede
SuriyaSammandamourthy <suriya.sammandamourthy@etu.u-pec.fr>
2020-04-21 21:18:08 +0200

    [ACCOUNT] add test file

---
+++ sc-account/src/test/java/pds/stardust/scaccount/ScAccountApplicationTests.java
@@ -0,0 +1,14 @@
+package pds.stardust.scaccount;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ScAccountApplicationTests {
+
+	@Test
+	public void test () {
+		assertTrue(true);
+	}
+
+}
