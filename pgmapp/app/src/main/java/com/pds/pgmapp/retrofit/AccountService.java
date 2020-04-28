0077f377841dcdf2784d0dbad55122dc806fb224
SuriyaSammandamourthy <suriya.sammandamourthy@etu.u-pec.fr>
2020-04-29 01:04:07 +0200

    [ACCOUNT] Add new retrofit service for user authentication

---
+++ pgmapp/app/src/main/java/com/pds/pgmapp/retrofit/AccountService.java
@@ -0,0 +1,25 @@
+package com.pds.pgmapp.retrofit;
+
+import com.pds.pgmapp.model.CustomerEntity;
+
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface AccountService {
+
+    @GET("account/")
+    Call<ResponseBody> heartbeat();
+
+    @Headers("Content-Type: application/json")
+    @POST("account/connect/")
+    Call<CustomerEntity> connect(@Body RequestBody customer);
+
+    @POST("account/update/token")
+    Call<CustomerEntity> updateToken(@Body CustomerEntity customer);
+
+}
