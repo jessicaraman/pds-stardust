fe8dd47784ee84e839be24f18b3888087c2ad9d5
Maxime Nguyen <lamson972@yahoo.fr>
2020-04-29 18:06:31 +0200

    [ADD] requirement for dockerfile

--- opencv-face-recognition/recognize_video.py
+++ opencv-face-recognition/recognize_video.py
@@ -1,3 +1,9 @@
+# USAGE
+# python recognize_video.py --detector face_detection_model \
+#	--embedding-model openface_nn4.small2.v1.t7 \
+#	--recognizer output/recognizer.pickle \
+#	--le output/le.pickle
+
 from imutils.video import VideoStream
 from imutils.video import FPS
 import numpy as np
