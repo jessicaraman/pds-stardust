72d1eda61206f52d900d8005e195468e7c8fb2e5
SuriyaSammandamourthy <suriya.sammandamourthy@etu.u-pec.fr>
2020-05-05 22:49:16 +0200

    add external constants class

---
+++ pgmapp/app/src/main/java/com/pds/pgmapp/handlers/Constant.java
@@ -0,0 +1,14 @@
+package com.pds.pgmapp.handlers;
+
+/**
+ * Constant class : handler all constants
+ */
+public class Constant {
+
+    // AccountActivity
+    public static final String TAG_ACCOUNT_ACTIVITY = "AccountActivity";
+
+    // Firebase
+    public static final String TAG_FIREBASE_HANDLER = "FirebaseHandler";
+    public static final String CHANNEL_ID = "channel_id";
+}
