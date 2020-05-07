5e4b640efc4db613c68eda4ed629be8dbdf35a41
samuel.lebon <samuel.lebon@etu.u-pec.fr>
2020-05-08 00:07:45 +0200

    [SSL] pgmapp : Activate secure communication

--- pgmapp/app/src/main/java/com/pds/pgmapp/retrofit/RetrofitInstance.java
+++ pgmapp/app/src/main/java/com/pds/pgmapp/retrofit/RetrofitInstance.java
@@ -1,6 +1,14 @@
 package com.pds.pgmapp.retrofit;
 
+import java.security.cert.CertificateException;
 import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
@@ -9,24 +17,64 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitInstance {
 
     private static Retrofit retrofit;
-    private static final String BASE_URL = "http://172.31.249.114:9980/api/";
+    private static final String BASE_URL = "https://172.31.249.114:9980/api/";
+    private static final String SSL_CONTEXT = "SSL";
+    private static final int CALL_TIMEOUT = 2;
+    private static final int CONNECT_TIMEOUT = 60;
+    private static final int READ_TIMEOUT = 10;
+    private static final int WRITE_TIMEOUT = 10;
 
     public static Retrofit getRetrofitInstance(){
         if(retrofit == null){
 
-            OkHttpClient.Builder httpClient = new OkHttpClient.Builder()
-                    .callTimeout(2, TimeUnit.MINUTES)
-                    .connectTimeout(60, TimeUnit.SECONDS)
-                    .readTimeout(10, TimeUnit.SECONDS)
-                    .writeTimeout(10, TimeUnit.SECONDS);
+            OkHttpClient httpClient = getUnsafeOkHttpClient();
 
             retrofit = new retrofit2.Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .client(httpClient.build())
+                    .client(httpClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
         return retrofit;
+    }
+
+    private static OkHttpClient getUnsafeOkHttpClient() {
+        try {
+
+            final TrustManager[] trustAllCerts = new TrustManager[] {
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new java.security.cert.X509Certificate[]{};
+                        }
+                    }
+            };
+
+            final SSLContext sslContext = SSLContext.getInstance(SSL_CONTEXT);
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            OkHttpClient.Builder builder = new OkHttpClient.Builder()
+                    .sslSocketFactory(sslSocketFactory, (X509TrustManager)trustAllCerts[0])
+                    .hostnameVerifier((hostname, session) -> true)
+                    .callTimeout(CALL_TIMEOUT, TimeUnit.MINUTES)
+                    .connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+                    .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+                    .writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS);
+
+            return builder.build();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
