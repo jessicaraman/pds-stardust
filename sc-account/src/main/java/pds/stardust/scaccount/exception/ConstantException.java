a6ac3abe543ae05c51fece9a245933ecd9cf34d2
SuriyaSammandamourthy <suriya.sammandamourthy@etu.u-pec.fr>
2020-05-07 16:56:54 +0200

    Add exception for getTokenByUsername function

--- sc-account/src/main/java/pds/stardust/scaccount/exception/ConstantException.java
+++ sc-account/src/main/java/pds/stardust/scaccount/exception/ConstantException.java
@@ -14,4 +14,7 @@ public class ConstantException {
     // updateToken
     public static final CustomException UPDATE_BAD_ID = new CustomException(2, "Update Token failure!", "Bad customer ID.");
     public static final CustomException UPDATE_AUTH_FAILURE = new CustomException(3, "Update Token failure!", "Bad username or password.");
+
+    // getToken
+    public static final CustomException GET_TOKEN_BAD_USERNAME = new CustomException(4, "Get Token failure!", "Bad username.");
 }
