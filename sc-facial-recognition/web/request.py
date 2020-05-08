000922b3fd126f7a4a242bac7bc30181fe80d11f
SuriyaSammandamourthy <suriya.sammandamourthy@etu.u-pec.fr>
2020-05-08 16:33:19 +0200

    add getToken function using http request to query remote REST API

--- sc-facial-recognition/web/request.py
+++ sc-facial-recognition/web/request.py
@@ -1,17 +1,18 @@
 # importing the requests library
 import requests
 
+# getToken from sc-account api
 def getToken(name):
+    resp = "none"
     # api-endpoint
     URL = "http://account:8085/token"
-
-    # location given here
-    location = "delhi technological universcity"
-
-    # defining a params dict for the parameters to be sent to the API
-    PARAMS = {'address': location}
-    customerEntity = { 'username' : name}
+    #URL = "https://172.31.249.114:9990/api/account/";
+    customerEntity = {'username': name}
     # sending get request and saving the response as response object
     r = requests.post(url=URL, json=customerEntity);
+    if(r.status_code == 200):
+        resp = r.text ### oui il a pas de token, c'est none par défaut
+    else:
+        print("no token for " + name)
 
-    return r.text
+    return resp
