4884332b4c73246fb8ade5296a9af5265fef757c
Damien <damien.baraton@hotmail.fr>
2020-05-11 15:29:23 +0200

    added tests for recently visited beacons

--- pgmapp/app/src/test/java/com/pds/pgmapp/ExampleUnitTest.java
+++ pgmapp/app/src/test/java/com/pds/pgmapp/ExampleUnitTest.java
@@ -1,6 +1,13 @@
 package com.pds.pgmapp;
 
+import androidx.lifecycle.Lifecycle;
+
+import com.pds.pgmapp.activity.BeaconActivity;
+import com.pds.pgmapp.model.RecentlyVisitedBeacons;
+
 import org.junit.Test;
+
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
@@ -10,13 +17,28 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void recentlyVisitedBeaconsMapReturnTrueWhenABeaconWasAlreadyVisited() {
+        String idBeacon = "ABC";
+        RecentlyVisitedBeacons recentlyVisitedBeacons = new RecentlyVisitedBeacons();
+        recentlyVisitedBeacons.put(idBeacon, new Date());
+        assertEquals(true, recentlyVisitedBeacons.isThisBeaconPresent(idBeacon));
     }
 
     @Test
-    public void allActive(){
+    public void recentlyVisitedBeaconsMapReturnTrueWhenABeaconWasRecentlyVisited() {
+        String idBeacon = "ABC";
+        RecentlyVisitedBeacons recentlyVisitedBeacons = new RecentlyVisitedBeacons();
+        recentlyVisitedBeacons.put(idBeacon, new Date());
+        assertEquals(true, recentlyVisitedBeacons.thisBeaconHasBeenVisitedRecently(idBeacon, new Date()));
+    }
 
+    @Test
+    public void recentlyVisitedBeaconsMapReturnFalseWhenABeaconWasVisitedLongAgo() {
+        String idBeacon = "ABC";
+        RecentlyVisitedBeacons recentlyVisitedBeacons = new RecentlyVisitedBeacons();
+        recentlyVisitedBeacons.put(idBeacon, new Date(System.currentTimeMillis() - (60 * 1000)));
+        assertEquals(false, recentlyVisitedBeacons.thisBeaconHasBeenVisitedRecently(idBeacon, new Date()));
     }
 }
