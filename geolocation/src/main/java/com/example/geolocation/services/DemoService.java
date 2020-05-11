f08a4185a4b2558d5d45ce1030327928a2af8313
samuel.lebon <samuel.lebon@etu.u-pec.fr>
2020-05-11 02:05:14 +0200

    [DEMO] Geolocation : Update demo service

--- geolocation/src/main/java/com/example/geolocation/services/DemoService.java
+++ geolocation/src/main/java/com/example/geolocation/services/DemoService.java
@@ -1,6 +1,8 @@
 package com.example.geolocation.services;
 
 import com.example.geolocation.beans.DekResponse;
+import com.example.geolocation.entities.LocationEntity;
+import com.example.geolocation.entities.LocationHistoryEntity;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import cryptolib.core.CryptoUtils;
 import cryptolib.core.StringUtils;
@@ -10,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -18,37 +19,46 @@ import org.springframework.web.client.RestTemplate;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @Service
-public class DemoService {
-
-    @Autowired
-    private RestTemplate restTemplate;
+public class DemoService extends AbstractRestService {
 
     @Value("${kms.base-url}")
     private String KMS_BASE_URL;
+
+    private final LocationHistoryService locationHistoryService;
 
     private final String DEK_RETRIEVAL_ENDPOINT = "keys/dek";
     private final String KEK_ROTATION_ENDPOINT = "keys/rotate";
     private final String MESSAGE = "PDS DEMO FOR ENCRYPTION AND DECRYPTION";
     private final String AES_ALGORITHM = "AES";
 
-    private Logger logger = LoggerFactory.getLogger(DemoService.class);
-    private ObjectMapper mapper = new ObjectMapper();
+    private final int LOCATION_HISTORY_SIZE = 5;
+    private final String LOCATION_HISTORY_ID = "demo";
 
-    public void demo() throws Exception {
+    private Logger logger = LoggerFactory.getLogger(DemoService.class);
+
+    @Autowired
+    public DemoService(RestTemplate restTemplate, ObjectMapper mapper, LocationHistoryService locationHistoryService) {
+        super(restTemplate, mapper);
+        this.locationHistoryService = locationHistoryService;
+    }
+
+    public void automatedDemo() throws Exception {
 
         final HttpEntity<Object> requestEntity = new HttpEntity<>(new HttpHeaders());
 
         logger.info("Send DEK retrieval request to KMS");
-        ResponseEntity<DekResponse> getDekResponse = restTemplate.exchange(KMS_BASE_URL + DEK_RETRIEVAL_ENDPOINT, HttpMethod.GET, requestEntity, DekResponse.class);
 
-        logger.info("Get status [{}] from KMS", getDekResponse.getStatusCode().toString());
+        String url = KMS_BASE_URL + DEK_RETRIEVAL_ENDPOINT;
+        ResponseEntity<DekResponse> getDekResponse = get(url, requestEntity, DekResponse.class);
 
         DekResponse getDekResponseBody = getDekResponse.getBody();
-
-        logger.info("Get response [{}] from KMS", mapper.writeValueAsString(getDekResponseBody));
 
         String dek = Objects.requireNonNull(getDekResponseBody).getData();
 
@@ -78,19 +88,19 @@ public class DemoService {
 
         logger.info("Rotating keys");
 
-        restTemplate.exchange(KMS_BASE_URL + KEK_ROTATION_ENDPOINT, HttpMethod.POST, requestEntity, String.class);
+        url = KMS_BASE_URL + KEK_ROTATION_ENDPOINT;
+
+        post(url, requestEntity, String.class);
 
         logger.info("Go check the KEK and DEK version on vault !");
         Thread.sleep(60000);
 
-        logger.info("Re send DEK retrieval request to KMS");
-        getDekResponse = restTemplate.exchange(KMS_BASE_URL + DEK_RETRIEVAL_ENDPOINT, HttpMethod.GET, requestEntity, DekResponse.class);
+        url = KMS_BASE_URL + DEK_RETRIEVAL_ENDPOINT;
 
-        logger.info("Get status [{}] from KMS", getDekResponse.getStatusCode().toString());
+        logger.info("Re send DEK retrieval request to KMS");
+        getDekResponse = get(url, requestEntity, DekResponse.class);
 
         getDekResponseBody = getDekResponse.getBody();
-
-        logger.info("Get response [{}] from KMS", mapper.writeValueAsString(getDekResponseBody));
 
         dek = Objects.requireNonNull(getDekResponseBody).getData();
 
@@ -110,4 +120,41 @@ public class DemoService {
 
     }
 
+    public Optional<LocationHistoryEntity> automatedDatabaseDemo() throws Exception {
+
+        logger.info("Clearing [locationHistoryEntity] collection !");
+        locationHistoryService.clearCollection();
+
+        logger.info("Go check that the collection is empty");
+        Thread.sleep(15000);
+
+        LocationHistoryEntity locationHistory = new LocationHistoryEntity();
+        locationHistory.setId(LOCATION_HISTORY_ID);
+
+        List<LocationEntity> locations = new ArrayList<>();
+
+        logger.info("Generating an location history !");
+
+        for (int i = 0; i < LOCATION_HISTORY_SIZE; i++) {
+            locations.add(new LocationEntity(i, -i, LocalDateTime.now().toString()));
+        }
+
+        locationHistory.setLocations(locations);
+
+        logger.info("Saving the location history");
+
+        locationHistoryService.save(locationHistory);
+
+        logger.info("Go check that the collection is not empty and encrypted");
+        Thread.sleep(15000);
+
+        logger.info("Finding the location history in database");
+        return locationHistoryService.findById(LOCATION_HISTORY_ID);
+
+    }
+
+    @Override
+    protected String getServiceName() {
+        return "KMS";
+    }
 }
