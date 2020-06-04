b79e4e2bdd017b8f7a09ca1d11dc7a7d26b62fe2
Damien <damien.baraton@hotmail.fr>
2020-06-04 21:59:33 +0200

    changed UC1 diagrams, renamed sc-frequentation micro service and added tests for recentlyVisitedBeacons

--- pgmapp/app/src/main/java/com/pds/pgmapp/model/RecentlyVisitedBeacons.java
+++ pgmapp/app/src/main/java/com/pds/pgmapp/model/RecentlyVisitedBeacons.java
@@ -15,4 +15,26 @@ public class RecentlyVisitedBeacons extends HashMap<String, Date> {
         return !(this.get(idBeacon).compareTo(new Date(currentDate.getTime() - 30 * 1000)) < 0);
     }
 
+    public boolean addBeacon(String idBeacon, Date currentDate) {
+        if (!isThisBeaconPresent(idBeacon)) {
+            this.put(idBeacon, currentDate);
+            return true;
+        } else {
+            if (!thisBeaconHasBeenVisitedRecently(idBeacon, currentDate)) {
+                if (this.get(idBeacon) != null) {
+                    this.remove(idBeacon);
+                }
+                this.put(idBeacon, currentDate);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Date getDateOfBeacon(String idBeacon) {
+        if(this.get(idBeacon) != null) {
+           return this.get(idBeacon);
+        }
+        return null;
+    }
 }
--- pgmapp/app/src/test/java/com/pds/pgmapp/frequentation_tests.java
+++ pgmapp/app/src/test/java/com/pds/pgmapp/frequentation_tests.java
@@ -16,29 +16,81 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class frequentation_tests {
 
     @Test
     public void recentlyVisitedBeaconsMapReturnTrueWhenABeaconWasAlreadyVisited() {
         String idBeacon = "ABC";
+        Date date = new Date();
         RecentlyVisitedBeacons recentlyVisitedBeacons = new RecentlyVisitedBeacons();
-        recentlyVisitedBeacons.put(idBeacon, new Date());
+        recentlyVisitedBeacons.addBeacon(idBeacon, date);
         assertEquals(true, recentlyVisitedBeacons.isThisBeaconPresent(idBeacon));
+    }
+
+    @Test
+    public void recentlyVisitedBeaconsMapReturnFalseWhenABeaconWasNeverVisited() {
+        String idBeacon = "ABC";
+        RecentlyVisitedBeacons recentlyVisitedBeacons = new RecentlyVisitedBeacons();
+        assertEquals(false, recentlyVisitedBeacons.isThisBeaconPresent(idBeacon));
     }
 
     @Test
     public void recentlyVisitedBeaconsMapReturnTrueWhenABeaconWasRecentlyVisited() {
         String idBeacon = "ABC";
+        Date date = new Date();
         RecentlyVisitedBeacons recentlyVisitedBeacons = new RecentlyVisitedBeacons();
-        recentlyVisitedBeacons.put(idBeacon, new Date());
-        assertEquals(true, recentlyVisitedBeacons.thisBeaconHasBeenVisitedRecently(idBeacon, new Date()));
+        recentlyVisitedBeacons.addBeacon(idBeacon, date);
+        assertEquals(true, recentlyVisitedBeacons.thisBeaconHasBeenVisitedRecently(idBeacon, date));
     }
 
     @Test
     public void recentlyVisitedBeaconsMapReturnFalseWhenABeaconWasVisitedLongAgo() {
         String idBeacon = "ABC";
+        Date date = new Date();
         RecentlyVisitedBeacons recentlyVisitedBeacons = new RecentlyVisitedBeacons();
-        recentlyVisitedBeacons.put(idBeacon, new Date(System.currentTimeMillis() - (60 * 1000)));
+        recentlyVisitedBeacons.addBeacon(idBeacon, new Date(date.getTime() - 60 * 1000));
         assertEquals(false, recentlyVisitedBeacons.thisBeaconHasBeenVisitedRecently(idBeacon, new Date()));
+    }
+
+    @Test
+    public void recentlyVisitedBeaconsMapDoNotKeepDatesOlderThanMostRecentOne() {
+        String idBeacon = "ABC";
+        Date dateFirstTimeSeen = new Date();
+        Date dateSecondTimeSeen = new Date(dateFirstTimeSeen.getTime() + 60 * 1000);
+        RecentlyVisitedBeacons recentlyVisitedBeacons = new RecentlyVisitedBeacons();
+        recentlyVisitedBeacons.addBeacon(idBeacon, dateFirstTimeSeen);
+        recentlyVisitedBeacons.addBeacon(idBeacon, dateSecondTimeSeen);
+        boolean test = dateFirstTimeSeen != recentlyVisitedBeacons.getDateOfBeacon(idBeacon) && dateSecondTimeSeen == recentlyVisitedBeacons.getDateOfBeacon(idBeacon);
+        assertTrue(test);
+    }
+
+    @Test
+    public void recentlyVisitedBeaconsMapCannotAddBeaconWhenSameOneWasVisitedTooRecently() {
+        String idBeacon = "ABC";
+        Date firstDate = new Date();
+        Date secondDate = new Date(firstDate.getTime() + 1000);
+        RecentlyVisitedBeacons recentlyVisitedBeacons = new RecentlyVisitedBeacons();
+        recentlyVisitedBeacons.addBeacon(idBeacon, firstDate);
+        assertEquals(false, recentlyVisitedBeacons.addBeacon(idBeacon, secondDate));
+    }
+
+    @Test
+    public void recentlyVisitedBeaconsMapCanAddBeaconWhenSameOneWasVisitedLongAgo() {
+        String idBeacon = "ABC";
+        Date firstDate = new Date();
+        Date secondDate = new Date(firstDate.getTime() + 60 * 1000);
+        RecentlyVisitedBeacons recentlyVisitedBeacons = new RecentlyVisitedBeacons();
+        recentlyVisitedBeacons.addBeacon(idBeacon, firstDate);
+        assertEquals(true, recentlyVisitedBeacons.addBeacon(idBeacon, secondDate));
+    }
+
+    @Test
+    public void recentlyVisitedBeaconsMapCanAddTwoDifferentBeaconsVisitedAroundTheSameTime() {
+        String idBeacon = "ABC";
+        String idBeacon2 = "XYZ";
+        Date date = new Date();
+        RecentlyVisitedBeacons recentlyVisitedBeacons = new RecentlyVisitedBeacons();
+        recentlyVisitedBeacons.addBeacon(idBeacon, date);
+        assertEquals(true, recentlyVisitedBeacons.addBeacon(idBeacon2, date));
     }
 }
