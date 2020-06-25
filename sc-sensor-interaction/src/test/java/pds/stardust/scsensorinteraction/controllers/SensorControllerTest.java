87eda1bfd91132b8dff155a99c653c8e84872c33
esuau <evan.suau@outlook.com>
2020-06-25 01:14:42 -0400

    test(controller): Add test coverage on getSensors() controller method

--- sc-sensor-interaction/src/test/java/pds/stardust/scsensorinteraction/controllers/SensorControllerTest.java
+++ sc-sensor-interaction/src/test/java/pds/stardust/scsensorinteraction/controllers/SensorControllerTest.java
@@ -1,16 +1,23 @@
 package pds.stardust.scsensorinteraction.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 import pds.stardust.scsensorinteraction.config.JasyptConfig;
 import pds.stardust.scsensorinteraction.entities.SensorEntity;
 import pds.stardust.scsensorinteraction.entities.TopicEntity;
 import pds.stardust.scsensorinteraction.exceptions.BadRequestException;
+import pds.stardust.scsensorinteraction.exceptions.ServiceException;
 import pds.stardust.scsensorinteraction.services.SensorService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -49,6 +56,42 @@ class SensorControllerTest {
 
         BadRequestException thrown = assertThrows(BadRequestException.class, () -> sensorController.create(null));
         assertEquals("sensor is null", thrown.getMessage());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestCasesForGetSensors")
+    void givenExistingSensor_whenGetSensors_thenSucceed(List<SensorEntity> entities) {
+        when(sensorService.list()).thenReturn(entities);
+
+        List<SensorEntity> actual = sensorService.list();
+
+        assertEquals(entities.size(), actual.size());
+    }
+
+    @Test
+    void givenServiceException_whenGetSensors_thenFail() {
+        when(sensorService.list()).thenThrow(new ServiceException("error when retreiving sensors"));
+
+        ServiceException thrown = assertThrows(ServiceException.class, () -> sensorController.getSensors());
+        assertEquals("error when retreiving sensors", thrown.getMessage());
+    }
+
+    private static Stream<Arguments> provideTestCasesForGetSensors() {
+        List<SensorEntity> data = new ArrayList<>();
+        TopicEntity topic1 = new TopicEntity("topic-1", "label");
+        SensorEntity sensor1 = new SensorEntity("sensor-1", topic1, "message");
+        TopicEntity topic2 = new TopicEntity("topic-2", "label");
+        SensorEntity sensor2 = new SensorEntity("sensor-2", topic2, "message");
+        TopicEntity topic3 = new TopicEntity("topic-3", "label");
+        SensorEntity sensor3 = new SensorEntity("sensor-3", topic3, "message");
+        data.add(sensor1);
+        data.add(sensor2);
+        data.add(sensor3);
+
+        return Stream.of(
+                Arguments.of(data),
+                Arguments.of(new ArrayList<>())
+        );
     }
 
 }
